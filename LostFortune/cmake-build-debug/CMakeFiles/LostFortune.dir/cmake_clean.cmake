006c138a39937c2daffbbf6c3be239ef36509d23
Attila Sedon <attila.sedon@dpc.hu>
2020-07-20 10:53:14 +0200

    Chapter 1 added

---
+++ LostFortune/cmake-build-debug/CMakeFiles/LostFortune.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LostFortune.dir/main.cpp.o"
+  "LostFortune"
+  "LostFortune.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LostFortune.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ LostFortune/main.cpp
@@ -0,0 +1,58 @@
+//Lost fortune
+// A Personalized adventure
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int threeScores() {
+    double s1, s2, s3;
+    cin >> s1;
+    cin >> s2;
+    cin >> s3;
+    cout << "average: " << (s1+s2+s3)/3 << endl;
+    return 0;
+}
+
+int main()
+{
+    threeScores();
+    cout << "Seven divided by three is " << 7.0 / 3 << endl;
+    const int GOLD_PIECES = 900;
+    int adventurers, killed, survivors;
+    string leader;
+
+    cout << "Welcome to Lost Fortune\n\n";
+    cout << "Please enter the following for your personalized adventure\n";
+    cout << "Enter a number: ";
+    cin >> adventurers;
+    cout << "Enter a number smaller, than the first: ";
+    cin >> killed;
+
+    survivors = adventurers - killed;
+
+    cout << "Enter your last name: ";
+    cin >> leader;
+
+    // tell the story
+    cout << "\nA brave group of " << adventurers << " set out on a quest ";
+    cout << "-- in search of the lost treasure of the Ancient Dwarves. ";
+    cout << "The group was led by that legendary rogue, " << leader << ".\n";
+
+    cout << "\nAlong the way, a band of marauding ogres ambushed the party. ";
+    cout << "All fought bravely under the command of " << leader;
+    cout << ", and the ogres were defeated, but at a cost. ";
+    cout << "Of the adventurers, " << killed << " were vanquished, ";
+    cout << "leaving just " << survivors << " in the group.\n";
+
+    cout << "\nThe party was about to give up all hope. ";
+    cout << "But while laying the deceased to rest, ";
+    cout << "they stumbled upon the buried fortune. ";
+    cout << "So the adventurers split " << GOLD_PIECES << " gold pieces.";
+    cout << leader << " held on to the extra " << (GOLD_PIECES % survivors);
+    cout << " pieces to keep things fair of course.\n";
+    return 0;
+}
